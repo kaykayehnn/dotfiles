d942a26ca33868550aab2fc71442689f48636d17
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-08-27 17:45:49 +0300

    Add less colors

---
+++ oh-my-zsh/less.zsh
@@ -0,0 +1,8 @@
+# Add colors to man pages
+export LESS_TERMCAP_mb=$'\E[1;31m'
+export LESS_TERMCAP_md=$'\E[1;36m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[1;32m'
+export LESS_TERMCAP_ue=$'\E[0m'
