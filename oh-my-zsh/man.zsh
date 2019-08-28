794d6be72540a4452c2f436f1a8f34bab36dace2
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-08-28 17:25:02 +0300

    Restrict less colors only to man pages
    Replace less.zsh with man.zsh

---
+++ oh-my-zsh/man.zsh
@@ -0,0 +1,8 @@
+# Add colors to man pages
+alias man="LESS_TERMCAP_mb=$'\E[1;31m' \
+  LESS_TERMCAP_md=$'\E[1;36m' \
+  LESS_TERMCAP_me=$'\E[0m' \
+  LESS_TERMCAP_so=$'\E[01;44;33m' \
+  LESS_TERMCAP_se=$'\E[0m' \
+  LESS_TERMCAP_us=$'\E[1;32m' \
+  LESS_TERMCAP_ue=$'\E[0m' man"
