81f54ecb012ca4da30418d249e62896df5ccf61d
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-10-18 18:19:36 +0300

    Improve man alias
    Handle shell builtins
    Formatting

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -4,13 +4,26 @@
 alias ping1="ping 1.1.1.1"
 alias batpkg="bat package.json"
 alias dot="cd ~/.dotfiles"
-# Add colors to man pages
-alias man="LESS_TERMCAP_mb=$'\E[1;31m' \
-  LESS_TERMCAP_md=$'\E[1;36m' \
-  LESS_TERMCAP_me=$'\E[0m' \
-  LESS_TERMCAP_so=$'\E[01;44;33m' \
-  LESS_TERMCAP_se=$'\E[0m' \
-  LESS_TERMCAP_us=$'\E[1;32m' \
-  LESS_TERMCAP_ue=$'\E[0m' man"
-
 alias weather="curl v2.wttr.in"
+
+# Augments man by adding colors and handling shell builtins.
+man() {
+  local COLORS="LESS_TERMCAP_mb=$'\E[1;31m' \
+    LESS_TERMCAP_md=$'\E[1;36m' \
+    LESS_TERMCAP_me=$'\E[0m' \
+    LESS_TERMCAP_so=$'\E[01;44;33m' \
+    LESS_TERMCAP_se=$'\E[0m' \
+    LESS_TERMCAP_us=$'\E[1;32m' \
+    LESS_TERMCAP_ue=$'\E[0m'"
+
+  for argument in "$@"; do
+    # If argument's man page resolves to the location for builtins,
+    if [ "$(command man --path -- "$1")" "==" "$(command man --path "builtin")" ]; then
+      # opens the builtins man page and searches for the first occurrence of
+      # the command with paragraph-like indentation.
+      eval "command man zshbuiltins | $COLORS less -p '^       $1 '"
+    else
+      eval "$COLORS command man -- '$argument'"
+    fi
+  done
+}
