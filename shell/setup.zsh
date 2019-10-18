1fb8412bff90778e724a5e029d0755520013e36b
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-10-18 23:47:08 +0300

    Optimize shell login performance
    Lazy loads slower commands
    Drops login time by 77% (1.3s -> 0.3s) 🎉

--- shell/setup.zsh
+++ shell/setup.zsh
@@ -5,9 +5,9 @@ export GPG_TTY="$(tty)"
 export BAT_CONFIG_PATH="$HOME/.dotfiles/.batrc"
 export RIPGREP_CONFIG_PATH="$HOME/.dotfiles/.ripgreprc"
 export GEM_HOME="$HOME/.gem"
+export NVM_DIR="$HOME/.nvm"
 
 eval "$(hub alias -s)"
-eval $(thefuck --alias)
 
 # Remove git's completions in favour of zsh's.
 # https://github.com/agross/dotfiles/commit/4938bc8987a5b4ef0c7411a2c4b988d89a3ade11
@@ -15,9 +15,31 @@ if [ -f /usr/local/share/zsh/site-functions/_git ]; then
   rm -f /usr/local/share/zsh/site-functions/_git
 fi
 
-# Setup nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" # This loads nvm
+# Helper to lazy load time-consuming commands. This only makes sense for shell
+# functions as other types of executables do not impact login performance.
+lazy() {
+  command=$1
+  init_function=$2
+
+  # After $command is called for the first time, $init_function automatically
+  # overrides this stub definition by defining the real function with the same
+  # name as $command.
+  eval "$command() {
+    eval '$init_function'
+    $command \$@
+    # unset -f '$init_function'
+  }"
+}
+
+init_nvm() {
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" # This loads nvm
+}
+lazy nvm init_nvm
+
+init_fuck() {
+  eval $(thefuck --alias)
+}
+lazy fuck init_fuck
 
 setup_fzf() {
   local FD_COMMAND="fd --hidden --exclude .git --exclude node_modules"
@@ -45,4 +67,4 @@ setup_fzf() {
 
 setup_fzf
 
-unset -f setup_fzf
+unset -f setup_fzf lazy
