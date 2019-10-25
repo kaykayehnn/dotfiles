1c2937f6b4a79a0f644046ed5021cbeeb1fdebaa
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-10-25 17:15:12 +0300

    Remove comment pointing to original source
    It would have been more appropriate in the commit message
    Leaving it here just in case https://github.com/agross/dotfiles/commit/4938bc8987a5b4ef0c7411a2c4b988d89a3ade11

--- shell/setup.zsh
+++ shell/setup.zsh
@@ -10,7 +10,6 @@ export NVM_DIR="$HOME/.nvm"
 eval "$(hub alias -s)"
 
 # Remove git's completions in favour of zsh's.
-# https://github.com/agross/dotfiles/commit/4938bc8987a5b4ef0c7411a2c4b988d89a3ade11
 if [ -f /usr/local/share/zsh/site-functions/_git ]; then
   rm -f /usr/local/share/zsh/site-functions/_git
 fi
