5949accfb4738072610feb3a2be10fc2716b73b3
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-10-26 21:38:24 +0300

    Fix tmux session naming bug in vscode-shell
    Directories with spaces weren't treated correctly

--- vscode/vscode-shell.sh
+++ vscode/vscode-shell.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Use directory name as session identifier and remove invalid characters.
-session_name="$(basename $(pwd) | sed 's/[^a-zA-Z0-9_-]//')"
+session_name="$(basename "$(pwd)" | sed 's/[^a-zA-Z0-9_-]//')"
 
 # -t name searches for sessions starting with name, whereas -t=name looks for
 # an exact match.
