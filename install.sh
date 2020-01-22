6cc7ea6a7e65ea8cd21ee8fddc29484b58413e25
Krasimir Nedelchev <kaykayehnn@gmail.com>
2020-01-22 23:29:37 +0200

    Refactor install.sh

--- install.sh
+++ install.sh
@@ -5,102 +5,109 @@
 # Set dotfiles dir in case we are running this script for the first time.
 DOTFILES="${DOTFILES:$HOME/.dotfiles}"
 
-# Check if brew is installed
-if ! command -v brew >/dev/null 2>&1; then
-  echo "Installing homebrew..."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+install_shell
+install_packages
 
-# Pull latest tap repositories
-brew update
-
-# Install deps from Brewfile
-brew bundle --file="$DOTFILES/Brewfile"
-
-NPM_GLOBAL_PACKAGES=(
-  create-react-app
-  gatsby-cli
-  jest
-  live-server
-  netlify-cli
-  nodemon
-  parrotsay
-  prettier
-  serve@^6
-  source-map-explorer
-  typescript
-  vtop
-  webpack-bundle-analyzer
-)
-
-# Install npm packages
-yarn global add ${NPM_GLOBAL_PACKAGES[@]}
-
-# Check if oh-my-zsh is installed
-if ! [ -e "$HOME/.oh-my-zsh" ]; then
-  echo "Installing oh-my-zsh..."
-  # RUNZSH=no skips entering zsh after installation to continue executing the
-  # rest of this script.
-  RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-: "${ZSH_CUSTOM:="$HOME/.oh-my-zsh/custom"}"
-# Check if powerlevel10k is installed
-if ! [ -e "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
-fi
-
-ZSH_PLUGINS=(
-  zsh-users/zsh-autosuggestions
-  zsh-users/zsh-syntax-highlighting
-  zsh-users/zsh-completions
-)
-
-# Install zsh plugins
-for plugin in "${ZSH_PLUGINS[@]}"; do
-  pluginPath="$ZSH_CUSTOM/plugins/$(basename $plugin)"
-  if ! [ -e "$pluginPath" ]; then
-    git clone --depth=1 "https://github.com/$plugin" "$ZSH_CUSTOM/plugins/$(basename $plugin)"
+install_shell() {
+  # Check if oh-my-zsh is installed
+  if ! [ -e "$HOME/.oh-my-zsh" ]; then
+    echo "Installing oh-my-zsh..."
+    # RUNZSH=no skips entering zsh after installation to continue executing the
+    # rest of this script.
+    RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   fi
-done
 
-# Install tmux plugin manager
-# https://github.com/tmux-plugins/tpm/blob/master/docs/automatic_tpm_installation.md
-if ! [ -d "$HOME/.tmux/plugins/tpm" ]; then
-  echo "Installing tmux plugins..."
-  git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
+  : "${ZSH_CUSTOM:="$HOME/.oh-my-zsh/custom"}"
+  # Check if powerlevel10k is installed
+  if ! [ -e "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+  fi
 
-# Install tmux plugins
-~/.tmux/plugins/tpm/bin/install_plugins
+  ZSH_PLUGINS=(
+    zsh-users/zsh-autosuggestions
+    zsh-users/zsh-syntax-highlighting
+    zsh-users/zsh-completions
+  )
 
-CODE_EXTENSIONS=(
-  CoenraadS.bracket-pair-colorizer
-  dbaeumer.vscode-eslint
-  eamodio.gitlens
-  EditorConfig.EditorConfig
-  esbenp.prettier-vscode
-  Gruntfuggly.todo-tree
-  johnpapa.vscode-peacock
-  msjsdiag.debugger-for-chrome
-  PKief.material-icon-theme
-  pnp.polacode
-  sdras.night-owl
-  VisualStudioExptTeam.vscodeintellicode
-)
+  # Install zsh plugins
+  for plugin in "${ZSH_PLUGINS[@]}"; do
+    pluginPath="$ZSH_CUSTOM/plugins/$(basename $plugin)"
+    if ! [ -e "$pluginPath" ]; then
+      git clone --depth=1 "https://github.com/$plugin" "$ZSH_CUSTOM/plugins/$(basename $plugin)"
+    fi
+  done
 
-for extension in "${CODE_EXTENSIONS[@]}"; do
-  # Without force vscode does not update already installed extensions
-  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code --install-extension "$extension" --force
-done
+  # Install tmux plugin manager
+  # https://github.com/tmux-plugins/tpm/blob/master/docs/automatic_tpm_installation.md
+  if ! [ -d "$HOME/.tmux/plugins/tpm" ]; then
+    echo "Installing tmux plugins..."
+    git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  fi
 
-# Check if iTerm2 shell integration is installed
-if ! [ -e "$HOME/.iterm2" ]; then
-  curl -fsSL https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
-fi
+  # Install tmux plugins
+  ~/.tmux/plugins/tpm/bin/install_plugins
+}
 
-# Update tldr pages
-tldr --update
+install_packages() {
+  # Check if brew is installed
+  if ! command -v brew >/dev/null 2>&1; then
+    echo "Installing homebrew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 
-# Install mkcert's Certificate Authority in the system trust store
-mkcert -install
+  # Pull latest tap repositories
+  brew update
+
+  # Install deps from Brewfile
+  brew bundle --file="$DOTFILES/Brewfile"
+
+  NPM_GLOBAL_PACKAGES=(
+    create-react-app
+    gatsby-cli
+    jest
+    live-server
+    netlify-cli
+    nodemon
+    parrotsay
+    prettier
+    serve@^6
+    source-map-explorer
+    typescript
+    vtop
+    webpack-bundle-analyzer
+  )
+
+  # Install npm packages
+  yarn global add ${NPM_GLOBAL_PACKAGES[@]}
+
+  CODE_EXTENSIONS=(
+    CoenraadS.bracket-pair-colorizer
+    dbaeumer.vscode-eslint
+    eamodio.gitlens
+    EditorConfig.EditorConfig
+    esbenp.prettier-vscode
+    Gruntfuggly.todo-tree
+    johnpapa.vscode-peacock
+    msjsdiag.debugger-for-chrome
+    PKief.material-icon-theme
+    pnp.polacode
+    sdras.night-owl
+    VisualStudioExptTeam.vscodeintellicode
+  )
+
+  for extension in "${CODE_EXTENSIONS[@]}"; do
+    # Without force vscode does not update already installed extensions
+    /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code --install-extension "$extension" --force
+  done
+
+  # Check if iTerm2 shell integration is installed
+  if ! [ -e "$HOME/.iterm2" ]; then
+    curl -fsSL https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+  fi
+
+  # Update tldr pages
+  tldr --update
+
+  # Install mkcert's Certificate Authority in the system trust store
+  mkcert -install
+}
