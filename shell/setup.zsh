24965552cda6ae2c391392b956cef0b907bd39b3
Krasimir Nedelchev <kaykayehnn@gmail.com>
2020-02-17 18:21:40 +0200

    Inline thefuck --alias instead of lazy loading it

--- shell/setup.zsh
+++ shell/setup.zsh
@@ -20,7 +20,23 @@ bindkey '^ ' autosuggest-execute
 [ -e "$HOME/.iterm2_shell_integration.zsh" ] && source "$HOME/.iterm2_shell_integration.zsh"
 
 setup_fuck() {
-  alias fuck='unalias fuck && eval $(thefuck --alias) && fuck'
+  # Inline `thefuck --alias` to improve performance.
+  fuck () {
+    TF_PYTHONIOENCODING=$PYTHONIOENCODING;
+    export TF_SHELL=zsh;
+    export TF_ALIAS=fuck;
+    TF_SHELL_ALIASES=$(alias);
+    export TF_SHELL_ALIASES;
+    TF_HISTORY="$(fc -ln -10)";
+    export TF_HISTORY;
+    export PYTHONIOENCODING=utf-8;
+    TF_CMD=$(
+        thefuck THEFUCK_ARGUMENT_PLACEHOLDER $@
+    ) && eval $TF_CMD;
+    unset TF_HISTORY;
+    export PYTHONIOENCODING=$TF_PYTHONIOENCODING;
+    test -n "$TF_CMD" && print -s $TF_CMD
+  }
 
   fuck-command-line() {
       local FUCK="$(THEFUCK_REQUIRE_CONFIRMATION=0 thefuck $(fc -ln -1 | tail -n 1) 2> /dev/null)"
