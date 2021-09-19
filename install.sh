237722e5d134e321527cfd8ad77c1b12d1c9af42
Krasimir Nedelchev <kaykayehnn@gmail.com>
2021-09-19 14:03:44 +0300

    Install gimp

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ install_packages() {
         signal-desktop nvme-cli obs-studio pinta kmag orca fd unrar httpie \
         copyq nnn youtube-dl git-delta chromium dog doge marktext lazygit \
         krusader dust bottom duf docker-compose cpufetch-git nmon awesome \
-        autokey-qt nextcloud-client
+        autokey-qt nextcloud-client gimp
     fi
   elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Check if brew is installed
