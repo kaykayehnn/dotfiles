508c046e931550be9f6872694d404fcf528662a7
Krasimir Nedelchev <kaykayehnn@gmail.com>
2021-11-07 22:49:00 +0200

    Install jq on Arch

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ install_packages() {
         krusader dust bottom duf docker-compose cpufetch-git nmon awesome \
         autokey-qt nextcloud-client gimp mongodb-bin mongodb-tools-bin \
         mongodb-compass caprine xbindkeys xautomation xorg-xev joplin-desktop \
-        tokei
+        tokei jq
       
       # DBeaver depends on jre11
       sudo yay -S dbeaver jre11-jdk
