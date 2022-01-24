5d5553d53edf1aed3abcc7306b5cb2dfce76834a
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2022-01-24 18:41:02 +0200

    Refactor and document bootstrap script better

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
-set -e
+# This script has to be sh-compatible, as some environments don't have bash
+# preinstalled (e.g. Alpine)
+
+# Pipefail is not available in sh.
+set -eu
 
 # Remember we cannot import any scripts here, as this script needs to be
 # pipeable.
@@ -9,6 +13,7 @@ DOTFILES="${DOTFILES:-$HOME/.dotfiles}"
 git clone --depth=1 https://github.com/kaykayehnn/dotfiles "$DOTFILES"
 cd "$DOTFILES"
 
+# TODO: check if bash is installed
 ./tools/link.sh
 ./tools/install.sh
 
