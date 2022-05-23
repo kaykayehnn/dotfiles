9eb89e56393900b1ebc7b2976483ac9904b008b6
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2022-05-23 10:07:32 +0300

    feat(brew): add brew shell variables

--- shell/setup.sh
+++ shell/setup.sh
@@ -78,6 +78,18 @@ setup_z() {
   }
 }
 
-setup_z
+setup_brew() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    export HOMEBREW_PREFIX="/opt/homebrew";
+    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+    export HOMEBREW_REPOSITORY="/opt/homebrew";
+    export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+    export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+  fi
+}
 
-unset -f setup_z
+setup_z
+setup_brew
+
+unset -f setup_z setup_brew
