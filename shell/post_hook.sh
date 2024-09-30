0f401b05ce70fa7b3d79a76900f3af4660cb0833
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2024-09-30 12:20:08 +0300

    feat(zsh): override gswc so that it can checkout existing branches

--- shell/post_hook.sh
+++ shell/post_hook.sh
@@ -12,3 +12,17 @@ less_termcap[so]=$'\E[01;44;33m'
 less_termcap[se]=$'\E[0m'
 less_termcap[us]=$'\E[1;32m'
 less_termcap[ue]=$'\E[0m'
+
+
+unalias gswc
+# Override gswc so that it can both create a new branch and checkout an existing branch.
+gswc() {
+  local branch=$1
+  if git show-ref --quiet --verify refs/heads/"$branch"; then
+    # Branch exists, switch to it
+    git switch "$branch"
+  else
+    # Branch does not exist, create and switch
+    git switch --create "$branch"
+  fi
+}
